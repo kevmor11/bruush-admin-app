0819c20bcb2d600bd124f49e156e269ec1cad61e
Kevin <admin@Garys-MBP.lan>
2018-06-04 17:31:26 -0700

    setup

---
+++ db.js
@@ -0,0 +1,11 @@
+require('dotenv').load();
+
+module.exports = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE_NAME
+  }
+});
--- index.js
+++ index.js
@@ -1,21 +1,121 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
-const app = express();
-const crypto = require('crypto');
-const cookie = require('cookie');
-const nonce = require('nonce')();
-const querystring = require('querystring');
-const request = require('request-promise');
+const dotenv = require('dotenv').config(),
+      express = require('express'),
+      app = express(),
+      crypto = require('crypto'),
+      cookie = require('cookie'),
+      nonce = require('nonce')(),
+      querystring = require('querystring'),
+      request = require('request-promise'),
+      { Model } = require('objection'),
+      Knex = require('knex');
 
-const apiKey = process.env.SHOPIFY_API_KEY;
-const apiSecret = process.env.SHOPIFY_API_SECRET;
-const scopes = 'read_products';
-const forwardingAddress = "{ngrok forwarding address}"; // Replace this with your HTTPS Forwarding address
+
+const knex = Knex({
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: 'example.db'
+  }
+});
+
+const apiKey = process.env.SHOPIFY_API_KEY,
+      apiSecret = process.env.SHOPIFY_API_SECRET,
+      scopes = 'read_products',
+      forwardingAddress = "https://ff7f40e8.ngrok.io"; // Replace this with your HTTPS Forwarding address
+
+const connection = require('db');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/shopify', (req, res) => {
+  const shop = req.query.shop;
+  if (shop) {
+    const state = nonce();
+    const redirectUri = forwardingAddress + '/shopify/callback';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+        'utf-8'
+      );
+    let hashEquals = false;
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+
+    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+    .then((accessTokenResponse) => {
+      const accessToken = accessTokenResponse.access_token;
+
+      const shopRequestUrl = 'https://' + shop + '/admin/shop.json';
+      const shopRequestHeaders = {
+        'X-Shopify-Access-Token': accessToken,
+      };
+
+      request.get(shopRequestUrl, { headers: shopRequestHeaders })
+      .then((shopResponse) => {
+        res.end(shopResponse);
+      })
+      .catch((error) => {
+        res.status(error.statusCode).send(error.error.error_description);
+      });
+    })
+    .catch((error) => {
+      res.status(error.statusCode).send(error.error.error_description);
+    });
+
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+// API ENDPOINT
+// https://ff7f40e8.ngrok.io/shopify?shop=bruushdev.myshopify.com
