8620d4f3fb54c8fc622a75cea8d29016b5b90ed3
Kevin <admin@Garys-MBP.lan>
2018-06-08 16:07:33 -0700

    setup

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const express = require('express'),
       compression = require('compression'),
       path = require('path'),
       bodyParser = require('body-parser'),
+      https = require('https'),
+      fs = require('fs'),
       connection = require('./db/connection'),
       sendMail = require('./util/MailUtil');
 
+// Settings view engine and middleware
 app.set('views', path.join(__dirname, 'view'))
    .set('view engine', 'ejs')
    .use(compression())
@@ -22,6 +25,7 @@ const products = require('./routes/products'),
 app.use('/products', products)
    .use('/discounts', discounts);
 
+// Knex config
 const knex = Knex({
   client: 'mysql',
   useNullAsDefault: true,
@@ -36,6 +40,20 @@ app.get('/', (req, res) => {
   res.redirect('/discounts');
 });
 
-app.listen(3000, () => {
+// Using custom ssl certificates in order to serve localhost over https
+// I've done this because when I try to access localhost:3000 in Chrome,
+  // it automically tries to serve localhost:3000 over https but it cannot without
+  // these certificates
+var certOptions = {
+  key: fs.readFileSync(path.resolve('./server.key')),
+  cert: fs.readFileSync(path.resolve('./server.crt'))
+};
+
+https.createServer(certOptions, app).listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+// We can switch back to this implementation prior to production
+// app.listen(3000, () => {
+//   console.log('Example app listening on port 3000!');
+// });
