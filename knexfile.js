c434fabae4d46acb06e35b88f17801b8f810f0f9
Kevin <admin@Garys-MBP.lan>
2018-06-08 13:45:26 -0700

    setup

--- knexfile.js
+++ knexfile.js
@@ -2,40 +2,14 @@
 require('dotenv').config();
 
 module.exports = {
-
-  development: {
-    client: 'mysql',
-    connection: {
-      filename: './db/connection'
-    }
+  client: 'mysql',
+  connection: {
+      host : process.env.DATABASE_HOST,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PASSWORD,
+      database : process.env.DATABASE_NAME
   },
-
-  staging: {
-    client: 'mysql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'mysql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations'
   }
-
 };
