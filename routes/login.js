bfa93c6b3f6e158d72843197880ec217733f783f
Kevin <admin@Garys-MBP.lan>
2018-06-12 15:07:31 -0700

    setup

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ const LoginController = require('../controller/LoginController'),
       router = require('express').Router()
 
 // Get all discount codes
-.get('/', isLoggedIn, LoginController.getLogin)
+.get('/', LoginController.getLogin)
 
 // Create a discount code
 .post('/',
--- util/isLoggedIn.js
+++ util/isLoggedIn.js
@@ -2,6 +2,6 @@ module.exports = (req, res, next) => {
   if (req.user) {
       next();
   } else {
-    res.redirect('/login');
+    res.render('login');
   }
 }
